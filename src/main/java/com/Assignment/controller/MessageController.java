e3a57b695bd3f4db0158346714860a5755b02bc2
pk65arya <pk65arya@gmail.com>
2023-08-19 15:25:13 +0530

    Name As PathVariable

--- src/main/java/com/Assignment/controller/MessageController.java
+++ src/main/java/com/Assignment/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.Assignment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,9 @@ public class MessageController {
     @GetMapping("/query")
     public String sayHello(@RequestParam String name){
         return "Hello "+name+"From BridgeLabz";
+    }
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name){
+        return "Hello "+name+ "From BridgeLabz";
     }
 }
