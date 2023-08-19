a4c7f294860c2dedaade5c3667957ef1246e782c
pk65arya <pk65arya@gmail.com>
2023-08-19 15:16:50 +0530

    Added:nameAsQuery

--- src/main/java/com/Assignment/controller/MessageController.java
+++ src/main/java/com/Assignment/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.Assignment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class MessageController {
     @GetMapping("/home")
     public String home(){
         return "Hello From BridgeLabz";
+    }
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello "+name+"From BridgeLabz";
     }
 }
